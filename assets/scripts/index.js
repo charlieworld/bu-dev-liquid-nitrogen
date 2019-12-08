1b205eda9e3fa1582a08a6a8c10d82e89d0a508d
Charlieeeee <charlieworld2008@gmail.com>
2019-12-08 23:31:41 +0800

    [DEV] product page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,28 @@
-console.log('test')
+
+document.body.onscroll = function() {scrollEvent()};
+
+function scrollEvent() {
+  var header = document.getElementById("nav_custom");
+
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    header.classList.add("nav-custom-fixed-top");
+  } else {
+    header.classList.remove("nav-custom-fixed-top");
+  }
+}
+
+
+function resizeEvent () {
+
+}
+
+
+function getWindowHeight() {
+  return  window.innerHeight;
+}
+
+function getWindowWidth() { 
+  return  window.innerWidth;
+}
+
+resizeEvent ();
