8e6a94f4174791201a57184b91a4f844c60c468f
Charlieeeee <charlieworld2008@gmail.com>
2019-12-17 01:00:26 +0800

    [Modified] header text

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,4 @@
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants";
 
 document.body.onscroll = function() {scrollEvent()};
 document.body.onresize = function() {resizeEvent()};
@@ -5,11 +6,18 @@ document.body.onresize = function() {resizeEvent()};
 
 function scrollEvent() {
   var header = document.getElementById("nav_custom");
+  var text = document.getElementsByClassName("nav-link")
 
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     header.classList.add("nav-custom-fixed-top");
+    for (var i=0;i<text.length;i++) {
+      text[i].style.textShadow = "none";
+    }
   } else {
     header.classList.remove("nav-custom-fixed-top");
+    for (var i=0;i<text.length;i++) {
+      text[i].style.textShadow = "0 2px 5px black";
+    }
   }
 }
 
